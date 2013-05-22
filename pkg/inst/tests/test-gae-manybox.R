0c1b5b4744a663623edb28c47ef83c4164651d5e
Karl Broman <kbroman@gmail.com>
2013-05-22 11:50:15 -0500

    Remove test-gae-manybox.R; didn't read the instructions completely.

--- pkg/inst/tests/test-gae-manybox.R
+++
@@ -1,5 +0,0 @@
-test_that("manyboxVis works on gae", {
-  testData <- t(matrix(rnorm(10000*100), nrow=100) + runif(100, 0, 2))
-  visDist <- manyboxVis(testData, gaeDevel=FALSE)
-  expect_that(visDist@serverID!="error", is_true())
-})
